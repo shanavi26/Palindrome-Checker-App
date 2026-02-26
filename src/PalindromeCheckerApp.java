dd6dc90f4735a9e8c42d018c72313c55d8b8f33f
shanavi26 <shanavi2626@gmail.com>
2026-02-26 15:04:08 +0530

    UC2:Hardcoded Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -10,5 +10,22 @@ public class PalindromeCheckerApp {
         System.out.println("Application started successfully.");
         System.out.println("Ready for palindrome operations...");
         System.out.println("========================================");
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i <= input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome");
+        } else {
+            System.out.println(input + " is not a palindrome");
+        }
     }
+
 }

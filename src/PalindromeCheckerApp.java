fad3261d882496b510a0c8df1f1971b96bf5057c
shanavi26 <shanavi2626@gmail.com>
2026-02-26 15:12:04 +0530

    UC4:Array Based Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -26,6 +26,29 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println(input + " is not a palindrome");
         }
+
+
+        //USE CASE 4
+        String input = "radar";
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
     }
 
 }

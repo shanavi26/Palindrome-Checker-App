4c18fc414c18ae61bad44b2289fde49dbbb69eae
shanavi26 <shanavi2626@gmail.com>
2026-03-06 15:09:30 +0530

    UC7:Dequeue Based

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,145 @@
+import java.util.Deque;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.LinkedList;
 public class PalindromeCheckerApp {
-}
+
+    public static void main(String[] args) {
+
+        System.out.println("==========================================");
+        System.out.println("   Welcome to Palindrome Checker System   ");
+        System.out.println("==========================================");
+        System.out.println("Version: 1.0");
+        System.out.println();
+        System.out.println("Application started successfully.");
+        System.out.println("Ready for palindrome operations...");
+        System.out.println("========================================");
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i <= input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome");
+        } else {
+            System.out.println(input + " is not a palindrome");
+        }
+
+        //USE CASE 3
+        String input = "madam";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a palindrome");
+        } else {
+            System.out.println(input + " is not a palindrome");
+
+            //USE CASE 4
+            String input = "radar";
+            char[] chars = input.toCharArray();
+            int start = 0;
+            int end = chars.length - 1;
+            boolean isPalindrome = true;
+
+            while (start < end) {
+                if (chars[start] != chars[end]) {
+                    isPalindrome = false;
+                    break;
+                }
+                start++;
+                end--;
+            }
+
+            if (isPalindrome) {
+                System.out.println(input + " is a palindrome.");
+            } else {
+                System.out.println(input + " is not a palindrome.");
+            }
+
+
+            //USE CASE 5
+            String input = "noon";
+            Stack<Character> stack = new Stack<>();
+
+            for (char c : input.toCharArray()) {
+                stack.push(c);
+            }
+
+            boolean isPalindrome = true;
+
+            for (char c : input.toCharArray()) {
+                if (c != stack.pop()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome) {
+                System.out.println(input + " is a palindrome.");
+            } else {
+                System.out.println(input + " is not a palindrome.");
+            }
+
+            //USE CASE 6
+            String input = "civic";
+
+            Queue<Character> queue = new LinkedList<>();
+            Stack<Character> stack = new Stack<>();
+
+            for (char c : input.toCharArray()) {
+                queue.add(c);
+                stack.push(c);
+            }
+
+            boolean isPalindrome = true;
+
+            while (!queue.isEmpty()) {
+                if (!queue.remove().equals(stack.pop())) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome) {
+                System.out.println(input + " is a palindrome.");
+            } else {
+                System.out.println(input + " is not a palindrome.");
+            }
+
+
+            //USE CASE 7
+            String input = "civic";
+
+            Deque<Character> deque = new LinkedList<>();
+
+            for (char c : input.toCharArray()) {
+                deque.add(c);
+            }
+
+            boolean isPalindrome = true;
+
+            while (deque.size() > 1) {
+                if (!deque.removeFirst().equals(deque.removeLast())) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome) {
+                System.out.println(input + " is a palindrome.");
+            } else {
+                System.out.println(input + " is not a palindrome.");
+            }
+        }
+
+    }
